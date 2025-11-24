5f2562ce4f4da35e686a0b7fea2e0f27cd650093
Yang Yue <124489072+yueyang0120@users.noreply.github.com>
2025-11-25 00:09:32 +0800

    Initial Commit

---
+++ ai-fortune-teller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ai-fortune-teller
+//
+//  Created by 杨越 on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ai-fortune-tellerTests/ai_fortune_tellerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ai_fortune_tellerTests.swift
+//  ai-fortune-tellerTests
+//
+//  Created by 杨越 on 25/11/25.
+//
+
+import Testing
+
+struct ai_fortune_tellerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
